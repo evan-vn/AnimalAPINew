06219a1d686c7aaad0aec6c9fb9c0ccd9daf32a3
Van Thai <thaithanhvan1993@gmail.com>
2024-10-10 16:39:12 -0400

    second commit

--- AnimalAPINew/src/main/java/com/animalapinew/AnimalAPINew/AnimalRepository.java
+++ AnimalAPINew/src/main/java/com/animalapinew/AnimalAPINew/AnimalRepository.java
@@ -3,10 +3,11 @@ package com.animalapinew.AnimalAPINew;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
-
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Integer> {
     List<Animal> getAnimalByAnimalCl(String animalCl);
 
